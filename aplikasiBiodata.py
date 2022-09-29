d2938de27cbceb841588adb1d56ee70d35ae2258
rhnsh <114548529+rhnsh@users.noreply.github.com>
2022-09-29 11:21:59 +0700

    Add files via upload

---
+++ aplikasiBiodata.py
@@ -0,0 +1,26 @@
+# aplikasi biodata create by raihan
+
+# input nama
+nama = input("masukkan nama anda!")
+# print / output Nama
+print("nama anda adalah : ",nama)
+
+# input NIM
+NIM = input("masukkan NIM anda!")
+# print / output NIM
+print("NIM anda adalah : ",NIM)
+
+# input kelas
+kelas = input("masukkan kelas anda!")
+# print / output kelas
+print("kelas anda adalah : ",kelas)
+
+# input prodi
+prodi = input("masukkan prodi anda!")
+# print / output prodi
+print("prodi anda adalah : ",prodi)
+
+# input tanggal lahir
+tanggallahir = input("masukkan tanggal lahir anda!")
+# print / output tanggal lahir
+print("tanggal lahir anda adalah : ",tanggallahir)
